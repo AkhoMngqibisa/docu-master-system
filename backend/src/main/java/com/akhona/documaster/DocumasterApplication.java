9eb298b28a6bcd9e78055681dfcd22e7d8cc6a3a
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-12-03 19:45:00 +0200

    Config - Backend: Create main class and refactor structure

---
+++ backend/src/main/java/com/akhona/documaster/DocumasterApplication.java
@@ -0,0 +1,13 @@
+package com.akhona.documaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumasterApplication.class, args);
+    }
+
+}
