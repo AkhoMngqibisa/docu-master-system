3dba7016ad85064ef3a1a0b400c9bb720558bf77
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-12-03 20:30:00 +0200

    Feat - Backend: Create User repository to find user by email

---
+++ backend/src/main/java/com/akhona/documaster/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.akhona.documaster.repository;
+
+import com.akhona.documaster.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
